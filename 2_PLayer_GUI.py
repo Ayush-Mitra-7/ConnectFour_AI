fedaab83c4d80b7fb92d6ed929db7be4fd095396
Ayush-Mitra-7 <ayushmitraofficial@gmail.com>
2023-09-28 14:38:21 +0530

    First Push

---
+++ 2_PLayer_GUI.py
@@ -0,0 +1,143 @@
+import numpy as np
+import pygame
+import sys
+import math
+
+BLUE = (57, 91, 100)
+BLACK = (39, 55, 77)
+RED = (200, 75, 49)
+YELLOW = (240, 222, 54)
+ORANGE = (255, 132, 0)
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+def create_board():
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+    return board
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT - 1][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
+                return (r, c), (r, c + 3)
+
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][c] == piece:
+                return (r, c), (r + 3, c)
+
+    # Check positively sloped diagonals
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece:
+                return (r, c), (r + 3, c + 3)
+
+    # Check negatively sloped diagonals
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
+                return (r, c), (r - 3, c + 3)
+
+    return None
+
+def draw_board(board, winning_move_coords=None):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, BLUE, (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (int(c * SQUARESIZE + SQUARESIZE / 2), int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+            elif board[r][c] == 2:
+                pygame.draw.circle(screen, YELLOW, (int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+
+    # Draw a box around the winning discs, if there is a winning move
+    if winning_move_coords:
+        r1, c1 = winning_move_coords[0]
+        r2, c2 = winning_move_coords[1]
+        pygame.draw.rect(screen, (255, 255, 0), (c1 * SQUARESIZE, (r1 + 1) * SQUARESIZE, (c2 - c1 + 1) * SQUARESIZE, SQUARESIZE), 5)
+
+    pygame.display.update()
+
+board = create_board()
+print_board(board)
+game_over = False
+turn = 0
+
+pygame.init()
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT + 1) * SQUARESIZE
+
+size = (width, height)
+
+RADIUS = int(SQUARESIZE / 2 - 5)
+
+screen = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+
+myfont = pygame.font.SysFont("monospace", 75)
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+            posx = event.pos[0]
+            if turn == 0:
+                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
+            else:
+                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS)
+            pygame.display.update()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+            posx = event.pos[0]
+            col = int(math.floor(posx / SQUARESIZE))
+
+            if is_valid_location(board, col):
+                row = get_next_open_row(board, col)
+                drop_piece(board, row, col, 1 if turn == 0 else 2)
+                winning_coords = winning_move(board, 1 if turn == 0 else 2)
+                if winning_coords:
+                    draw_board(board, winning_coords)
+                    label = myfont.render("Player {} wins!!".format(1 if turn == 0 else 2), 1, RED if turn == 0 else YELLOW)
+                    screen.blit(label, (40, 10))
+                    pygame.display.update()
+                    game_over = True
+
+                print_board(board)
+                draw_board(board)
+
+                turn += 1
+                turn = turn % 2
+
+            if game_over:
+                pygame.time.wait(3000)
+
+    # Move the display update outside the event loop to refresh the screen after each loop iteration.
+    pygame.display.update()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ConnectFour_AI
-Created a AI using Heuristic search and MinMax Algorithm to play against Human in the game of Connect Four
+
+Created a AI using Heuristic search and MinMax Algorithm to play against Human in the game of Connect Four. Used Pygame library to create the Interface for the game. Defined games contraints and rules and also generated state space for the MinMax algorithm at a deapth of 6 for the AI to make the best possible move based on the Heuristic values of the Players moves.
+
+Start of the game
+
+![GUI_start](screenshots\connect_Four_1.png)
+
+End of the game
+
+![GUI_start](screenshots\connect_Four_2.png)
